ad18a1f30d86629f4e1af7bfccab4c939007290a
mcrc2659 <m.anchia.18@gmail.com>
2023-06-05 22:24:53 -0600

    Creacion del proyecto Blog TKCR

---
+++ src/main/java/com/BlogTKCR/BlogTkcrApplication.java
@@ -0,0 +1,13 @@
+package com.BlogTKCR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogTkcrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogTkcrApplication.class, args);
+	}
+
+}
